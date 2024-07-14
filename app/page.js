f34b4727e164e46aafcb641650b015237b0ed6d9
GanymedeNil <ganymedenil@gmail.com>
2024-07-14 14:45:34 +0800

    -设置字体

--- app/page.js
+++ app/page.js
@@ -96,7 +96,7 @@ export default function Home() {
           </div>
         </header>
 
-        <main className="container mx-auto py-12 px-4">
+        <main className="container mx-auto py-12 px-4 font-serif">
           <section className="mb-16">
             <div className="bg-white p-6 rounded-lg shadow-md">
               <h2 className="text-2xl font-semibold mb-4">背景</h2>
